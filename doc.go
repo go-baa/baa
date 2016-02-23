f9ac8b34ac4811f2dd9f88c97f39684e7fafa920
coldstar <safeie@163.com>
2016-02-24 00:37:27 +0800

    optimize baa use di

---
+++ doc.go
@@ -0,0 +1,18 @@
+// baa  
+//
+/*
+   package main
+
+   import (
+       "github.com/go-baa/baa"
+   )
+
+   func main() {
+       app := baa.Classic()
+       app.Get("/", func(c *baa.Context) {
+           c.String(200, "Hello World!")
+       })
+       app.Run(":8001")
+   }
+*/
+package baa
