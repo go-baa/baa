b3ea948ddf65d865983fcccf6dc93ece059a87d9
coldstar <safeie@163.com>
2016-12-01 19:25:36 +0800

    fix data race

--- di.go
+++ di.go
@@ -1,5 +1,9 @@
 package baa
 
+import (
+	"sync"
+)
+
 // DIer is an interface for baa dependency injection
 type DIer interface {
 	Set(name string, v interface{})
@@ -9,6 +13,7 @@ type DIer interface {
 // DI provlider a dependency injection service for baa
 type DI struct {
 	store map[string]interface{}
+	rw    sync.RWMutex
 }
 
 // NewDI create a DI instance
@@ -21,10 +26,15 @@ func NewDI() DIer {
 // Set register a di
 // baa dependency injection must be the special interface
 func (d *DI) Set(name string, v interface{}) {
+	d.rw.Lock()
 	d.store[name] = v
+	d.rw.Unlock()
 }
 
 // Get fetch a di by name, return nil when name not set.
 func (d *DI) Get(name string) interface{} {
-	return d.store[name]
+	d.rw.RLock()
+	v := d.store[name]
+	d.rw.RUnlock()
+	return v
 }
